f74f901ac1161287d6ec6c57afb0fb9e614aeb13
Mubbashir Ali <mubbashirali35@gmail.com>
2020-08-22 02:33:23 +0500

    build removed from .gitignore

---
+++ client/build/precache-manifest.0cab0f9de5598312965ec401da4098ed.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03c18ae968b3c7adb975cc123b00fa05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b04faefde59d97a9878",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "c101e8e1bc11c9a45043",
+    "url": "/static/css/main.014a537a.chunk.css"
+  },
+  {
+    "revision": "2b04faefde59d97a9878",
+    "url": "/static/js/2.c8834f93.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.c8834f93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c101e8e1bc11c9a45043",
+    "url": "/static/js/main.5fd720c1.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "a82040ecf06139df2f3e653326b3420c",
+    "url": "/static/media/Bangers-Regular.a82040ec.ttf"
+  },
+  {
+    "revision": "bc4242a60a6c29d59cec2eefe6d9be2d",
+    "url": "/static/media/avatar_image.bc4242a6.jpg"
+  }
+]);
